6e8ccc0974396806dbe9ed9bffd25f3ddf56ee71
Kaleb Lucas <klucas765@gmail.com>
2020-01-22 19:27:36 -0600

    Completed main mission.

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,42 @@ public class BalancedBracketsTest {
     @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+
+    @Test
+    public void testOneBracketLeft(){
+        String oneBracketLeft = "[";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(oneBracketLeft));
+    }
+
+    @Test
+    public void testStringEndsWithClosedBracket(){
+        String oneBracketRight = "]";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(oneBracketRight));
+    }
+
+    @Test
+    public void testEmptyString(){
+        String emptyString = "";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(emptyString));
+    }
+
+    @Test
+    public void testOpposingBrackets(){
+        String opposingBrackets = "][";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(opposingBrackets));
+    }
+
+    @Test
+    public void testMultipleClosedBrackets(){
+        String multipleClosedBrackets = "[]]";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(multipleClosedBrackets));
+    }
+
+    @Test
+    public void testMultipleOpenBrackets(){
+        String multipleOpenBrackets = "[[]";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(multipleOpenBrackets));
     }
 
 
